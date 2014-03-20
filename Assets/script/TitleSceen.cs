9a524187c162ec10c590b10a9c4fde1d7ecb8127
loveiset <loveiset@qq.com>
2014-03-20 21:32:40 +0800

    with start window

---
+++ Assets/script/TitleSceen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleSceen : MonoBehaviour {
+
+    void OnGUI()
+    {
+        GUI.skin.label.fontSize = 48;
+
+        GUI.skin.label.alignment = TextAnchor.LowerCenter;
+
+        GUI.Label(new Rect(0, 30, Screen.width, 100), "太空大战");
+
+        if (GUI.Button(new Rect(Screen.width * 0.5f - 100, Screen.height * 0.7f, 200, 30), "开始游戏"))
+        {
+            Application.LoadLevel("level1");
+        }
+    }
+
+}
